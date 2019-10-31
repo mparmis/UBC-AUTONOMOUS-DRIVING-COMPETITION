9f29324d3343e7ed8d9766c49b7096f435aef6cb
jwhite2a <justjoshwhite@gmail.com>
2019-10-30 17:11:02 -0700

    semi-working image viewer

---
+++ view_camera.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+import roslib
+import sys
+import rospy
+import cv2
+import numpy as np
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+from geometry_msgs.msg import Twist
+import matplotlib.pyplot as plt
+
+class image_converter:
+
+  def __init__(self):
+    self.image_sub = rospy.Subscriber("/R1/pi_camera/image_raw",Image,self.callback)
+    self.bridge = CvBridge()
+    #self.ax1 = plt.subplot(1,2,1)
+
+
+  def callback(self,data):
+    try:
+      cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+      print("got image!")
+    except CvBridgeError as e:
+      print(e)
+
+    ##image processing:
+    imgplot = plt.imshow(cv_image)
+    plt.show()  # display it
+    #cv2.imshow('window', cv_image)
+   
+
+def main(args):
+  ic = image_converter()
+  rospy.init_node('image_converter', anonymous=True)
+  try:
+    rospy.spin()
+  except KeyboardInterrupt:
+    print("Shutting down")
+  plt.close('all')
+
+if __name__ == '__main__':
+    main(sys.argv)
